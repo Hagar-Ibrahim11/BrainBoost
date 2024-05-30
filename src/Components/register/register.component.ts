dd44abe778b4c9004483c2401085eb92bfe704bf
hagar <hagaribra04@gmail.com>
2024-05-31 01:06:36 +0300

    bind lgin and register with API

--- src/Components/register/register.component.ts
+++ src/Components/register/register.component.ts
@@ -1,20 +1,35 @@
 import { Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-
-import { faUser, faEnvelope, faLock, faKey } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../../Services/auth.service';
+import { Router } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [    FontAwesomeModule],
+   imports: [FormsModule,HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  constructor(library: FaIconLibrary) {
-    // Add icons to the library for convenient access in templates
-    library.addIcons(faUser, faEnvelope, faLock, faKey);
-  }
+
+  user: any = {email: '', password: '',userName:'',firstName:'',lastName:''};
+  role:string='';
+constructor(private router: Router, private http: HttpClient){}
+register() {
+  console.log(this.user,this.role);
+  this.http.post(`http://localhost:5079/api/Account/register?role=${this.role}`, this.user, { responseType: 'text' }).subscribe(
+    response => {
+      console.log('Registration successful:', response);
+      this.router.navigate(['/login']);
+    },
+    error => {
+      console.error('Registration failed:', error);
+      
+    }
+  );
+}
+
 }
--- src/Services/auth.service.ts
+++ src/Services/auth.service.ts
@@ -2,18 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { json } from 'stream/consumers';
+import { jwtDecode } from 'jwt-decode';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  userData=null;
   constructor(private http: HttpClient, private router: Router) { }
-  login(credentials: { email: string, password: string }): Observable<any> {
-    return this.http.post<any>(``, credentials);
+  decodeUserData()
+  {
+    let encodedToken=JSON.stringify(this.getToken());
+    let decodedToken:any= jwtDecode(encodedToken);
+    console.log(decodedToken);
+    this.userData=decodedToken;
+  }
+  login(credentials: { userName: string, password: string }): Observable<any> {
+    return this.http.post<any>(`http://localhost:5079/api/Account/login`, credentials);
   }
 
-  register(user: any): Observable<any> {
-    return this.http.post<any>(``, user);
+  register(user: any,role :string): Observable<any> {
+    return this.http.post<any>(`http://localhost:5079/api/Account/register?role=${role}`, user);
   }
 
   logout(): void {
