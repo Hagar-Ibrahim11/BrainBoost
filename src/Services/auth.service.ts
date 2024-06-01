3068aacd0dee4599d2d8fd5a24c24d38f8c2338c
tomas wagdy <tomaswagdy549>
2024-06-01 04:17:13 +0300

    Add validation in login and register using reactive forms

--- src/Services/auth.service.ts
+++ src/Services/auth.service.ts
@@ -2,28 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { json } from 'stream/consumers';
 import { jwtDecode } from 'jwt-decode';
+import { environment } from '../Enviroment/enviroment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  userData=null;
-  constructor(private http: HttpClient, private router: Router) { }
-  decodeUserData()
-  {
-    let encodedToken=JSON.stringify(this.getToken());
-    let decodedToken:any= jwtDecode(encodedToken);
+  userData = null;
+  constructor(private http: HttpClient, private router: Router) {}
+  decodeUserData() {
+    let encodedToken = JSON.stringify(this.getToken());
+    let decodedToken: any = jwtDecode(encodedToken);
     console.log(decodedToken);
-    this.userData=decodedToken;
+    this.userData = decodedToken;
   }
-  login(credentials: { userName: string, password: string }): Observable<any> {
-    return this.http.post<any>(`http://localhost:5079/api/Account/login`, credentials);
+  login(credentials: { userName: string; password: string }): Observable<any> {
+    return this.http.post<any>(
+      `${environment.baseUrl}/api/Account/login`,
+      credentials
+    );
   }
 
-  register(user: any,role :string): Observable<any> {
-    return this.http.post<any>(`http://localhost:5079/api/Account/register?role=${role}`, user);
+  register(user: any, role: string): Observable<any> {
+    return this.http.post<any>(
+      `${environment.baseUrl}/api/Account/register?role=${role}`,
+      user
+    );
   }
 
   logout(): void {
