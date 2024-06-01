0351d2b9c1ecd69a2a90b0555418894fd6e2a96c
khloudfarouk <[khloudfarouk360@gmail.com]>
2024-06-01 18:39:26 +0300

    Pricing UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from '../Components/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
+  imports: [RouterOutlet,HomeComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LayoutComponent } from '../Components/layout/layout.component';
 import { HomeComponent } from '../Components/home/home.component';
 import { CoursesComponent } from '../Components/courses/courses.component';
 import { CourseDetailsComponent } from '../Components/course-details/course-details.component';
+import { PricingComponent } from '../Components/pricing/pricing.component';
 import { NotFoundComponent } from '../Components/not-found/not-found.component';
 
 export const routes: Routes = 
@@ -16,10 +17,14 @@ export const routes: Routes =
       path: '',
       component: LayoutComponent,
       children: [
+        
         { path: 'home', component: HomeComponent },
+       
         { path: 'courses', component: CoursesComponent },
         { path: 'courseDetails', component: CourseDetailsComponent },
       ]
     },
+    { path: 'pricing', component: PricingComponent },
+
     { path:"**",component:NotFoundComponent},
   ];
