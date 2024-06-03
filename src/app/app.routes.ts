d214e19a3f1e0b2b6661af26c28c3c32a259d9fd
osama-ibrahem <asamtabrahym870@gmail.com>
2024-06-03 21:23:22 +0300

    course details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,25 @@ import { HomeComponent } from '../Components/home/home.component';
 import { CoursesComponent } from '../Components/courses/courses.component';
 import { CourseDetailsComponent } from '../Components/course-details/course-details.component';
 import { NotFoundComponent } from '../Components/not-found/not-found.component';
+import { AddCourseComponent } from '../Components/add-course/add-course.component';
+import { AddVideoComponent } from '../Components/add-video/add-video.component';
+import { AddQuizComponent } from '../Components/add-quiz/add-quiz.component';
 
-export const routes: Routes = 
+export const routes: Routes =
 [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'courseDetails', component: CourseDetailsComponent },
+    { path: 'addCourse',
+     component: AddCourseComponent,
+     children:[
+      { path: 'addVideo', component: AddVideoComponent },
+      { path: 'addQuiz', component: AddQuizComponent },
+      { path: 'addCertificate', component: AddCourseComponent },
+     ]
+    },
+
     {
       path: '',
       component: LayoutComponent,
