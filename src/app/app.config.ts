6799441d92724a74635f1cfb283304bc8bfedc75
khloudfarouk <[khloudfarouk360@gmail.com]>
2024-06-24 18:46:32 +0300

    latest

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,6 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideHttpClient(),
     provideAnimationsAsync(),
-    provideAnimationsAsync(),
+    provideAnimationsAsync(), provideAnimationsAsync(),
   ],
 };
