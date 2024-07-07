ec4bc8e7f9c8767f662c766f975b54281442f732
hagar <hagaribra04@gmail.com>
2024-07-07 23:16:23 +0300

    pagination

--- src/Components/teachers-for-admin/teachers-for-admin.component.ts
+++ src/Components/teachers-for-admin/teachers-for-admin.component.ts
@@ -3,44 +3,45 @@ import { ITeacher } from '../../models/iteacher';
 import { Router } from '@angular/router';
 import { AdminDashboardServiceService } from '../../Services/AdminDashboard/admin-dashboard-service.service';
 import { CommonModule } from '@angular/common';
-
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-teachers-for-admin',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxPaginationModule],
   templateUrl: './teachers-for-admin.component.html',
-  styleUrl: './teachers-for-admin.component.css'
+  styleUrls: ['./teachers-for-admin.component.css']
 })
 export class TeachersForAdminComponent implements OnInit {
+  ListOfTeachers: ITeacher[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 8;
 
-constructor(
-  private router:Router,
-  private admindashboardservice:AdminDashboardServiceService
+  constructor(
+    private router: Router,
+    private admindashboardservice: AdminDashboardServiceService
+  ) {}
 
-){}
+  ngOnInit(): void {
+    this.GetAllTeachers();
+  }
 
-ListOfTeachers:ITeacher[]=[]
-ngOnInit(): void {
-  this.GetAllTeachers()
-}
-GetAllTeachers()
-{
-  this.admindashboardservice.GetAllTeachers().subscribe(
-    (data: ITeacher[]) => {
-      this.ListOfTeachers = data;
-    },
-    (error) => {
-      console.error('Error fetching Teachers', error);
-    }
-  );
-}
-GoToTeacherDetails(id: Number) {
-  this.router.navigate(['/TeacherDetails', id]);
-}
+  GetAllTeachers() {
+    this.admindashboardservice.GetAllTeachers().subscribe(
+      (data: ITeacher[]) => {
+        this.ListOfTeachers = data;
+      },
+      (error) => {
+        console.error('Error fetching Teachers', error);
+      }
+    );
+  }
 
-DeleteTeacher(TeacherId:Number)
-  {
+  GoToTeacherDetails(id: number) {
+    this.router.navigate(['/TeacherDetails/', id]);
+  }
+
+  DeleteTeacher(TeacherId: number) {
     this.admindashboardservice.DeleteTeacher(TeacherId).subscribe(
       () => {
         this.GetAllTeachers();
@@ -49,6 +50,5 @@ DeleteTeacher(TeacherId:Number)
         console.error(error);
       }
     );
-
   }
 }
